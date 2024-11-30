81027e2ae16b48e005f693761cf490de1ca6999c
naol <naolmeseret22@gmail.com>
2024-11-30 23:05:06 +0300

    i added mongodb package and write envariment variable

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASEPASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connection);
+    console.log('connected succsusfuly');
+  });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`server starts at ${port} port number`);
