e895f0d2988ebd779b1f84e4a28003446a6e8e51
naol <naolmeseret22@gmail.com>
2024-12-02 21:36:55 +0300

    adding tour model

--- server.js
+++ server.js
@@ -18,6 +18,25 @@ mongoose
     console.log(con.connection);
     console.log('connected succsusfuly');
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.7,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have proce'],
+  },
+});
+
+const tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`server starts at ${port} port number`);
